87cc364a653e0c90728c6967544e2720404eb058
Default1997 <andrey.soldatkin@mail.ru>
2021-02-16 02:44:22 +0300

    Add project

---
+++ frontend/models/ManageMessages.php
@@ -0,0 +1,17 @@
+<?php
+namespace frontend\models;
+ 
+use yii\base\Model;
+ 
+ 
+class ManageMessages extends Model
+{
+	public $id_message;
+
+	public function rules()
+	{
+	    return [
+	        [['id_message'], 'required']
+	    ];
+	} 	
+}
